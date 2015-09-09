8c421d62c0178baa12daef40f143cef55338429c
cyrilledelabre <cyrilledelabre@users.noreply.github.com>
2015-09-09 09:06:06 -0300

    Delete conf.php

--- config/conf.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    return array(
-        'default' => array(
-            'host' => 'localhost:8889',
-            'database' => 'cyrilledwacdel',
-            'login' => 'root',
-            'password' => 'root')
-    );
